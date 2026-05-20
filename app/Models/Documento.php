49652e0254a247a08c73864ed2abdd54e3fa135b
figueromefer <ffigueroa@boveda-creativa.com>
2026-05-20 09:35:13 -0600

    Add inquilino relation to Documento

--- app/Models/Documento.php
+++ app/Models/Documento.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Documento extends Model {
+class Documento extends Model
+{
     use HasFactory;
 
     protected $table = 'documentos';
@@ -16,16 +17,24 @@ class Documento extends Model {
     protected $fillable = [
         'fk_cliente',
         'fk_propiedad',
+        'fk_inquilino',
         'titulo',
         'tipo',
         'archivo',
     ];
 
-    public function cliente() {
+    public function cliente()
+    {
         return $this->belongsTo(Cliente::class, 'fk_cliente', 'pk_cliente');
     }
 
-    public function propiedad() {
+    public function propiedad()
+    {
         return $this->belongsTo(Propiedad::class, 'fk_propiedad', 'pk_propiedad');
+    }
+
+    public function inquilino()
+    {
+        return $this->belongsTo(Inquilino::class, 'fk_inquilino', 'id');
     }
 }
